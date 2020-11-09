3f4fc91bab1b1e5af26a8eb6e974758ce9b547d8
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-09 11:22:21 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
+    <div>
+      test
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
