9ff3f4d45af992f36923b6b7d4ec3fe4db98e1c4
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-10 13:35:41 +0000

    Commit

--- src/components/Main/Deposits/DepositElements.js
+++ src/components/Main/Deposits/DepositElements.js
@@ -2,9 +2,22 @@ import styled from 'styled-components';
 
 
 export const DepositsTitle = styled.h1`
-
+    font-weight: 500;
+    color: ${({theme}) => theme.textColor};
+    font-size: 1.3rem;
+    display: flex;
+    align-items: center;
 `
 
 export const DepositsCount = styled.div`
-
-`
+  margin-left: 1rem;
+  font-size: 1rem;
+  background-color: ${({ theme }) => theme.header};
+  color: ${({ theme }) => theme.headerNumber};
+  width: 28px;
+  height: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px
+`;
--- src/components/Main/Deposits/Deposits.js
+++ src/components/Main/Deposits/Deposits.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { DepositsCount, DepositsTitle } from './DepositElements'
 
-const Deposits = ({ title , count, }) => {
-  return <div></div>;
+const Deposits = ({ title , count, data }) => {
+  return(
+      <div>
+          <DepositsTitle>{title}<DepositsCount>{count}</DepositsCount></DepositsTitle>
+      </div>
+    ) 
 };
 
 export default Deposits
