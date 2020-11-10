8751dd7717de5e69214fd7e9bbecda4ca03836b8
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-10 12:24:15 +0000

    Commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,13 +4,15 @@ import { MainContainer } from './MainElemetns';
 import Nav from './Nav';
 import NewDepositBtn from './NewDepositBtn';
 
+import depositData from '../../DepositData.json'
+
 const Main = () => {
   return (
     <MainContainer>
       <Nav />
       <NewDepositBtn />
-      <Deposits title="Depositis" count={2} data={} />
-      <Deposits title="Depositis" count={2} data={} />
+      <Deposits title="Active Depositis" count={2} data={depositData.active} />
+      <Deposits title="Closed Depositis" count={2} data={depositData.closed} />
     </MainContainer>
   );
 };
