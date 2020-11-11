1d671a1871e7b79a026412d8d0bc3f1b8942f875
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-11 12:47:51 +0000

    Commit

--- src/components/Main/Deposits/Deposits.js
+++ src/components/Main/Deposits/Deposits.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DepositsCount, DepositsTitle } from './DepositElements';
 import SortingBar from './SortingBar';
-import Deposit from './Deposit'
+import Deposit from './Deposit';
 
 const Deposits = ({ title, count, data }) => {
   return (
@@ -11,8 +11,8 @@ const Deposits = ({ title, count, data }) => {
         <DepositsCount>{count}</DepositsCount>
       </DepositsTitle>
       <SortingBar />
-      {data.map(deposit => (
-        <Deposit data={deposit} key={deposit.property.address.street}/>
+      {data.map((deposit) => (
+        <Deposit data={deposit} key={deposit.property.address.street} />
       ))}
     </div>
   );
