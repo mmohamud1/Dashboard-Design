53d37036bb0c12d8ea102512dad6573a65cc08d3
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-12 11:41:42 +0000

    Commit

--- src/components/Main/Deposits/Deposit.js
+++ src/components/Main/Deposits/Deposit.js
@@ -1,5 +1,18 @@
 import React from 'react'
-import { DepositContainer, DepositMoveInDate, DepositProperty, DepositPropertyImg, DepositPropertyStreet, DepositPropertyText, DepositRent, DepositStatus, DepositSubtitle, DepositText, DepositWrapper } from './DepositElements';
+import {
+  DepositContainer,
+  DepositMoveInDate,
+  DepositProperty,
+  DepositPropertyImg,
+  DepositPropertyStreet,
+  DepositPropertyText,
+  DepositRent,
+  DepositStatus,
+  DepositSubtitle,
+  DepositText,
+  DepositWrapper,
+  DepositStatusIndicator,
+} from './DepositElements';
 import Image from '../../../assets/images/home-icon.png'
 
 const Deposit = ({data}) => {
@@ -8,9 +21,7 @@ const Deposit = ({data}) => {
     return (
       <DepositContainer>
         <DepositProperty>
-          <DepositPropertyImg
-            src={Image}
-          />
+          <DepositPropertyImg src={Image} />
           <DepositPropertyText>
             <DepositPropertyStreet>
               {property.address.street}
@@ -23,11 +34,23 @@ const Deposit = ({data}) => {
         <DepositMoveInDate>{moveInDate}</DepositMoveInDate>
         <DepositRent>£{rent}</DepositRent>
         <DepositWrapper>
-            <DepositText>£{deposit.amount}</DepositText>
-            <DepositSubtitle>{deposit.type}</DepositSubtitle>
+          <DepositText>£{deposit.amount}</DepositText>
+          <DepositSubtitle>{deposit.type}</DepositSubtitle>
         </DepositWrapper>
         <DepositStatus>
-            <DepositText>{status.message}</DepositText>
+          <DepositText>{status.message}</DepositText>
+          {(() => {
+            switch (status.level) {
+              case 1:
+                return <DepositStatusIndicator color='#F17E7E' />;
+              case 2:
+                return <DepositStatusIndicator color='#FFD056' />;
+              case 3:
+                return <DepositStatusIndicator color='#75C282' />;
+              default:
+                return <DepositStatusIndicator color='#AAA5A5' />;
+            }
+          })()}
         </DepositStatus>
       </DepositContainer>
     );
