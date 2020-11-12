974a7c6add4146734412734f8078c9fcb0c7bdf3
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-12 11:33:10 +0000

    Commit

--- src/components/Main/Deposits/Deposit.js
+++ src/components/Main/Deposits/Deposit.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { DepositContainer } from './DepositElements';
+import { DepositContainer, DepositMoveInDate, DepositProperty, DepositPropertyImg, DepositPropertyStreet, DepositPropertyText, DepositSubtitle } from './DepositElements';
+import Image from '../../../assets/images/home-icon.png'
 
 const Deposit = ({data}) => {
 
     const { property, moveInDate, rent, deposit, status } = data;
     return (
-        <DepositContainer>
-            Deposits
-        </DepositContainer>
-    )
+      <DepositContainer>
+        <DepositProperty>
+          <DepositPropertyImg
+            src={Image}
+          />
+          <DepositPropertyText>
+            <DepositPropertyStreet>
+              {property.address.street}
+            </DepositPropertyStreet>
+            <DepositSubtitle>
+              {property.address.city} {property.address.state}
+            </DepositSubtitle>
+          </DepositPropertyText>
+        </DepositProperty>
+        <DepositMoveInDate>{moveInDate}</DepositMoveInDate>
+      </DepositContainer>
+    );
 }
 
 export default Deposit
