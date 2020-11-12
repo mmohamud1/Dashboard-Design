9e4d46c970adde66a69b8f8c13a1387164cb9ec7
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-12 11:48:35 +0000

    Commit

--- src/components/Main/Deposits/Deposit.js
+++ src/components/Main/Deposits/Deposit.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   DepositContainer,
   DepositMoveInDate,
@@ -13,47 +13,46 @@ import {
   DepositWrapper,
   DepositStatusIndicator,
 } from './DepositElements';
-import Image from '../../../assets/images/home-icon.png'
+import Image from '../../../assets/images/home-icon.png';
 
-const Deposit = ({data}) => {
+const Deposit = ({ data }) => {
+  const { property, moveInDate, rent, deposit, status } = data;
+  return (
+    <DepositContainer>
+      <DepositProperty>
+        <DepositPropertyImg src={Image} />
+        <DepositPropertyText>
+          <DepositPropertyStreet>
+            {property.address.street}
+          </DepositPropertyStreet>
+          <DepositSubtitle>
+            {property.address.city} {property.address.state}
+          </DepositSubtitle>
+        </DepositPropertyText>
+      </DepositProperty>
+      <DepositMoveInDate>{moveInDate}</DepositMoveInDate>
+      <DepositRent>£{rent}</DepositRent>
+      <DepositWrapper>
+        <DepositText>£{deposit.amount}</DepositText>
+        <DepositSubtitle>{deposit.type}</DepositSubtitle>
+      </DepositWrapper>
+      <DepositStatus>
+        <DepositText>{status.message}</DepositText>
+        {(() => {
+          switch (status.level) {
+            case 1:
+              return <DepositStatusIndicator color='#F17E7E' />;
+            case 2:
+              return <DepositStatusIndicator color='#FFD056' />;
+            case 3:
+              return <DepositStatusIndicator color='#75C282' />;
+            default:
+              return <DepositStatusIndicator color='#AAA5A5' />;
+          }
+        })()}
+      </DepositStatus>
+    </DepositContainer>
+  );
+};
 
-    const { property, moveInDate, rent, deposit, status } = data;
-    return (
-      <DepositContainer>
-        <DepositProperty>
-          <DepositPropertyImg src={Image} />
-          <DepositPropertyText>
-            <DepositPropertyStreet>
-              {property.address.street}
-            </DepositPropertyStreet>
-            <DepositSubtitle>
-              {property.address.city} {property.address.state}
-            </DepositSubtitle>
-          </DepositPropertyText>
-        </DepositProperty>
-        <DepositMoveInDate>{moveInDate}</DepositMoveInDate>
-        <DepositRent>£{rent}</DepositRent>
-        <DepositWrapper>
-          <DepositText>£{deposit.amount}</DepositText>
-          <DepositSubtitle>{deposit.type}</DepositSubtitle>
-        </DepositWrapper>
-        <DepositStatus>
-          <DepositText>{status.message}</DepositText>
-          {(() => {
-            switch (status.level) {
-              case 1:
-                return <DepositStatusIndicator color='#F17E7E' />;
-              case 2:
-                return <DepositStatusIndicator color='#FFD056' />;
-              case 3:
-                return <DepositStatusIndicator color='#75C282' />;
-              default:
-                return <DepositStatusIndicator color='#AAA5A5' />;
-            }
-          })()}
-        </DepositStatus>
-      </DepositContainer>
-    );
-}
-
-export default Deposit
+export default Deposit;
